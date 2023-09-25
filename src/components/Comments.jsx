b394d9d577277f6e0174762cd0134e2ed91b6652
lolaeurich <lola.tatti@gmail.com>
2023-09-25 16:31:46 -0300

    feat: sessão de comentários finalizada

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,10 +1,39 @@
 import React from "react"
+import mapa from "/src/assets/mapa.png"
+import comentarios from "/src/assets/comentarios.png"
 
 const Comments = () => {
     return(
         <>
         <section className="bg-gray-200 drop-shadow-md w-full h-[500px]">
-           <div></div>
+           <div className="container flex flex-row justify-between align-center">
+            <div className="relative basis-1/2">
+                <img src={mapa} alt="" className={'z-0 w-full h-[480px] right-6 pt-3'}></img>
+             <div className="absolute top-16 left-10 w-full h-[450px]">   
+                <img src={comentarios} alt="" className={"pr-10"}></img>
+             </div>   
+            </div>
+            <div className="direction-column basis-1/2 pl-12 pb-12 pt-20 content-center items-center">
+                <h1 className="mb-5 text-3xl font-bold">O que nossos usuários dizem?</h1>
+                <p className="mb-3 text-base font-normal">
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipisicing elit.
+                    Ab accusamus, aliquid assumenda atque.<br/>
+                    <br/>
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipisicing elit.
+                    Ab accusamus, aliquid assumenda atque.<br/>
+                    <br/>
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipisicing elit.
+                    Ab accusamus, aliquid assumenda atque.<br/>
+                    <br/>
+                    Lorem ipsum dolor sit amet, 
+                    consectetur adipisicing elit.
+                    Ab accusamus, aliquid assumenda atque.<br/>
+                </p>
+            </div>
+           </div>
         </section>
         </>
     )
