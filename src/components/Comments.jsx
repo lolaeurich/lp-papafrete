5fe2f62b9dcd421d9132c0eeb0baff0695dc04b5
lolaeurich <lola.tatti@gmail.com>
2023-09-25 16:37:45 -0300

    feat: sessão de comentários finalizada

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -9,11 +9,11 @@ const Comments = () => {
            <div className="container flex flex-row justify-between align-center">
             <div className="relative basis-1/2">
                 <img src={mapa} alt="" className={'z-0 w-full h-[480px] right-6 pt-3'}></img>
-             <div className="absolute top-16 left-10 w-full h-[450px]">   
+             <div className="absolute top-12 left-10 w-full h-[450px]">   
                 <img src={comentarios} alt="" className={"pr-10"}></img>
              </div>   
             </div>
-            <div className="direction-column basis-1/2 pl-12 pb-12 pt-20 content-center items-center">
+            <div className="direction-column basis-1/2 pl-12 pt-20 content-center items-center">
                 <h1 className="mb-5 text-3xl font-bold">O que nossos usuários dizem?</h1>
                 <p className="mb-3 text-base font-normal">
                     Lorem ipsum dolor sit amet, 
