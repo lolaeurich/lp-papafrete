e0e3cc895b8c4f56ae63c5aaa3ef7240e6e992de
lolaeurich <lola.tatti@gmail.com>
2023-10-09 10:32:03 -0300

    fix: altura

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,14 +9,14 @@ const Navbar = () => {
   // const HandleRemovePopUp = () => setOpenPopup(false);
 
   return (
-     <div id="nav" className="drop-shadow-md lg:w-full max-w-screen-2xl">
-        <div className="flex items-center justify-between bg-white py-4 md:px-16 px-7">
+     <div id="nav" className="w-[237%] drop-shadow-md lg:w-full max-w-screen-2xl">
+        <div className="flex items-center justify-between bg-white py-4 px-16 md:px-16">
           <div className="cursor-pointer">
-            <img className={'h-7 lg:h-14 xl:h-18'} src="/assets/logonova.png" alt=""/>
+            <img className={'h-28 md:h-7 lg:h-14 xl:h-18'} src="/assets/logonova.png" alt=""/>
           </div>
           <div className="md:flex md:items-center space-x-5 md:ml-4 uppercase cursor-pointer">
-            <button className="bg-white border-solid border-2 border-yellow-400 shadow-md shadow-gray-600/40 hover:bg-yellow-500 text-black font-normal hover:text-white py-1 px-8 text-sm rounded-xl xl:text-xl">Login</button>
-            <button  className="bg-yellow-500 shadow-md shadow-gray-600/40 hover:bg-white text-black font-normal hover:text-black py-1 px-3 text-sm rounded-xl xl:text-xl">Cadastre-se</button>
+            <button className="bg-white border-solid border-2 border-yellow-400 shadow-md shadow-gray-600/40 hover:bg-yellow-500 text-black font-normal hover:text-white rounded-xl text-3xl py-3 px-4 md:py-1 md:px-8 md:text-sm  xl:text-xl">Login</button>
+            <button  className="bg-yellow-500 shadow-md shadow-gray-600/40 hover:bg-white text-black font-normal hover:text-black rounded-xl text-3xl py-3 px-4 md:py-1 md:px-3 md:text-sm xl:text-xl">Cadastre-se</button>
           </div>
           {/* <PopUp openPopUp={openPopup} closePopUp={HandleRemovePopUp} /> */}
         </div>
