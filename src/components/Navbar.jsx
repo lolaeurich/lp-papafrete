0c8d979f141299533e7cd3c05f5f28dbea706d1e
lolaeurich <lola.tatti@gmail.com>
2023-10-16 13:40:23 -0300

    README update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,12 +9,12 @@ const Navbar = () => {
   // const HandleRemovePopUp = () => setOpenPopup(false);
 
   return (
-     <div id="nav" className="w-16 drop-shadow-md max-w-screen-2xl">
+     <div id="nav" className="drop-shadow-md min-h-[6rem] w-full">
         <div className="flex items-center justify-between bg-white py-4 px-16 md:px-16">
           <div className="cursor-pointer">
             <img className={'h-28 md:h-7 lg:h-14 xl:h-18'} src="/assets/logonova.png" alt=""/>
           </div>
-          <div className="md:flex md:items-center space-x-5 md:ml-4 uppercase cursor-pointer">
+          <div className="flex items-end md:flex md:items-center space-x-5 md:ml-4 uppercase cursor-pointer">
             <button className="bg-white border-solid border-2 border-yellow-400 shadow-md shadow-gray-600/40 hover:bg-yellow-500 text-black font-normal hover:text-white rounded-xl text-3xl py-3 px-4 md:py-1 md:px-8 md:text-sm  xl:text-xl">Login</button>
             <button  className="bg-yellow-500 shadow-md shadow-gray-600/40 hover:bg-white text-black font-normal hover:text-black rounded-xl text-3xl py-3 px-4 md:py-1 md:px-3 md:text-sm xl:text-xl">Cadastre-se</button>
           </div>
